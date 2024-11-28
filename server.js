f059841fec08ee5e5fcac934f099723bbebf7260
Le Khai Tri <lekhaitri.2003@gmail.com>
2024-11-28 17:33:17 +0700

    add description

--- server.js
+++ server.js
@@ -1,65 +1,63 @@
-var express = require("express")
+var express = require("express");
+var { createHandler } = require("graphql-http/lib/use/express");
+var { buildSchema } = require("graphql");
 
-var { createHandler } = require("graphql-http/lib/use/express")
-
-var { buildSchema } = require("graphql")
-// Construct a schema, using GraphQL schema language
+// tao schema
 var schema = buildSchema(`
     type User {
-    id: ID!
-    name: String!
-    email: String!
+        id: ID!
+        username: String!
+        description: String!
     }
     type Query {
-    hello: String
-    getUser(id: ID!): User
-    getUsers: [User]
+        getUser(id: ID!): User
+        getUsers: [User]
     }
     type Mutation {
-    createUser(name: String!, email: String!): User
-    updateUser(id: ID!, name: String, email: String): User
+        createUser(username: String!, description: String!): User
+        updateDescription(id: ID!, description: String!): User
     }
-    `);// The root provides a resolver function for each API endpoint
-    var users = [];
-    var idCounter = 1;
-    var root = {
-    hello() {
-    return "Hello world!";
-    },
-    createUser({ name, email }) {
-    const user = { id: idCounter++, name, email };
-    users.push(user);
-    return user;
-    },
-    updateUser({ id, name, email }) {
-    const user = users.find((user) => user.id == id);
-    if (!user) {
-    throw new Error("User not found");
-    }
-    if (name) {
-    user.name = name;
-    }
-    if (email) {
-    user.email = email;
-    }
-    return user;
-    },
+`);
+
+// in-memory db
+var users = [];
+var idCounter = 1;
+
+// xu ly tung api
+var root = {
     getUser({ id }) {
-    return users.find((user) => user.id == id);
+        return users.find((user) => user.id == id);
     },
     getUsers() {
-    return users;
+        return users;
     },
-    };
-var app = express()
-// Create and use the GraphQL handler.
+    createUser({ username, description }) {
+        const user = { id: idCounter++, username, description };
+        users.push(user);
+        return user;
+    },
+    updateDescription({ id, description }) {
+        const user = users.find((user) => user.id == id);
+        if (!user) {
+            throw new Error("User not found");
+        }
+        user.description = description;
+        return user;
+    },
+};
+
+var app = express();
+
+// tao handler
 app.all(
-"/graphql",
-createHandler({
-schema: schema,
-rootValue: root,
-})
-)
-// Start the server at port
-app.listen(4000)
-console.log("Running a GraphQL API server at http://localhost:4000/graphql")
+    "/graphql",
+    createHandler({
+        schema: schema,
+        rootValue: root,
+    })
+);
+
+// chay server 
+app.listen(4000, () => {
+    console.log("Running a GraphQL API server at http://localhost:4000/graphql");
+});
